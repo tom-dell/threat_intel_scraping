7ef0c904dc0929ee193a2109641fe73e55640869
Tom <thomas.dell@outlook.com.au>
2018-05-26 16:03:44 +1000

    Update circl_osint_feed.py

--- circl_osint_feed.py
+++ circl_osint_feed.py
@@ -2,29 +2,53 @@ import urllib.request
 import filecmp
 import difflib
 import re 
+import os
+import subprocess
 
-#site = urllib.request.urlretrieve('https://www.circl.lu/doc/misp/feed-osint/', 'day1.txt')
-
+#this function cleans the download of the webpage, so only the json file names remain, then saves it as daily.txt
 def cleanFile(file):
-    rfile = open(file, 'r')
-    
-    #i = 1
+    rfile = open(file, 'r+')
+    tfile = open("daily.txt", "w+")
     for line in rfile:
         t = re.search(r'(?<=json">)(.*)(?=\<\/a>)', line)
         if t is not None:
-            print(t.group(0))
-        #print(str(i) + " - " + line)
-        #i+=1
+            tfile.writelines(t.group(0) + '\n')
     rfile.close()
+    tfile.close()
 
-
-
+#this function compares the main file (dld_circle_osint_feed), with the second file (daily), and writes all missing json file names to download.txt
 def compare():
-    result = filecmp.cmp('day1.txt', 'day2.txt')
+    result = filecmp.cmp('dld_circle_osint_feed.txt', 'daily.txt')
+    d = open('messy_download.txt', 'w+')
     if result == True:
-        print ('yes')
+        print ('Nothing to download')
     else:
-        for line in difflib.unified_diff(open('day1.txt').readlines(), open('day2.txt').readlines(), n=0):
-            print(str(line))
+        for line in difflib.unified_diff(open('dld_circle_osint_feed.txt').readlines(), open('daily.txt').readlines(), n=0):
+            d.writelines(str(line))
+    d.close()
 
-cleanFile('prac.txt')
+#this function will clean the messy download file resulting in a text of commands to run to download the missing files
+def cleanMDL():
+    mdl = open('messy_download.txt', 'r')
+    dl = open('download.sh', 'w+')
+    a = open('dld_circle_osint_feed.txt', 'a')
+    for line in mdl:
+        t = re.search(r'(?<=\+)[^m](.*)(.json)', line)
+        if t is not None:
+            a.writelines(t.group(0) + '\n')
+            dl.writelines("wget https://www.circl.lu/doc/misp/feed-osint/" + t.group(0) + " -O /home/circl_osint_feed/" + '\n' + '''curl -X POST "localhost:9200/circl_osint_feed/_doc/" -H 'Content-Type: application.json' --data-binary "@''' + t.group(0) + '''"''' + "\n")
+    mdl.close()
+    dl.close()
+                
+
+#the script downloads the webpage (as dump), cleans it and saves it as daily, then compares the main file with the daily file, then deletes dump
+
+site = urllib.request.urlretrieve('https://www.circl.lu/doc/misp/feed-osint/', 'dump.txt')
+cleanFile('dump.txt')
+compare()
+os.remove('dump.txt')
+cleanMDL()
+os.remove('messy_download.txt')
+os.remove('daily.txt')
+subprocess.call(['./download.sh'])
+#os.remove('download.sh')
