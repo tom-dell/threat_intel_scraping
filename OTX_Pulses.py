52b31c8253240d8b2324f6c6ae981ed5022f0988
Tom <thomas.dell@outlook.com.au>
2018-06-06 10:46:29 +1000

    Update OTX_Pulses.py

--- OTX_Pulses.py
+++ OTX_Pulses.py
@@ -5,29 +5,43 @@ import os
 import subprocess
 import requests
 
-def get_id():
-    
-    w = open('test.txt', 'w+')
-    l = open('log.txt', 'w+')
+#Put your OTX API key in between the two apostrophe's on line 17
 
+#this function uses OTX's API to download /u/AlienVault's latest posts, and extracts the IDs of /u/ALienVault's pulses in the most convoluted way humanly possible
+def todays_IDs():
+    #opens required files
+    page_dump = open('page_dump.txt', 'w+')
+    messy_todays_IDs = open('messy_todays_IDs.txt', 'w+') 
 
-    headers = {'X-OTX-API-KEY': 'd9d2976d89b7288d4499f6d54630a6985e324be75fa286b338c2d31b5d0d6334',}
+    #runs the API call, and saves the text body as page_dump
+    headers = {'X-OTX-API-KEY': '',}
     response = requests.get('https://otx.alienvault.com/api/v1/pulses/user/AlienVault', headers=headers)
+    page_dump.write(response.text)
+    page_dump.close()
     
-    l.write(response.text)
-    l.close()
+    #uses regex to extract the IDs from page_dump, saved them to messy_todays_IDs, then closes messy_todays_IDs, and page_dump, then deletes the page_dump file
+    for line in page_dump:
+        IDs = re.findall(r'(?<="id": ")(.*?)(?=")', line)
+        messy_todays_IDs.write(str(IDs))
+    page_dump.close()
+    os.remove('page_dump.txt')
+    messy_todays_IDs.close()
+
+    #opens the messy_todays_IDs file, and removes the commas, square bracks, quotes, and lines that have less than 5 characters, leaving only the legitimate pulse IDs, 
+    #then writes those pulse IDs to todays_IDs, closes both files, and deletes the messy_todays_IDs file
+    #this finally leaves us with the file we will compare to our master list of already downloaded pulses
+    messy_todays_IDs = open('messy_todays_IDs.txt', 'r')
+    todays_IDs = open('todays_IDs', 'w+')    
+    for line in messy_todays_IDs:
+        todays_IDs.write(line.replace(',', '\n'))
+    messy_todays_IDs.close()
+    os.remove('messy_todays_IDs.txt')
+    todays_IDs.close()
+
+def compare_the_market():
+    print('here we will compare the master list against todays_IDs')
 
 
-def what():
-    q = open('log.txt', 'r') 
-    l2 = open('log2.txt', 'w+')    
-    for line in q:
-        s = re.search(r'(?<="id": ")(.*?)(?=")', line)
-        l2.write(s)
-    q.close()
-    l2.close()
 
 
-
-get_id()
-what()
+todays_IDs()
